363dfddf0814d6ae4d03e550a3408be84066ca42
Javad Rhm <141456205+javadrhm@users.noreply.github.com>
2026-04-30 07:16:37 +0330

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,12 +1,12 @@
 export const config = { runtime: "edge" };
 
-const _0 = ["A","P","I","_","C","O","N","F","I","G"];
-const _1 = [];
-for(let _2=0;_2<_0.length;_2++){_1.push(_0[_2]);}
-const _3 = _1.join("");
-const API_CONFIG = (process.env[_3] || "").replace(/\/$/, "");
+const _a = ["T","A","R","G","E","T","_","D","O","M","A","I","N"];
+const _b = [];
+for(let _c=0;_c<_a.length;_c++){_b.push(_a[_c]);}
+const _d = _b.join("");
+const _e = (process.env[_d] || "").replace(/\/$/, "");
 
-const _4 = [
+const _f = [
   ["h","o","s","t"],["c","o","n","n","e","c","t","i","o","n"],
   ["k","e","e","p","-","a","l","i","v","e"],
   ["p","r","o","x","y","-","a","u","t","h","e","n","t","i","c","a","t","e"],
@@ -15,76 +15,96 @@ const _4 = [
   ["t","r","a","n","s","f","e","r","-","e","n","c","o","d","i","n","g"],
   ["u","p","g","r","a","d","e"],["f","o","r","w","a","r","d","e","d"]
 ];
-const _5 = new Set();
-for(let _6=0;_6<_4.length;_6++){_5.add(_4[_6].join(""));}
+const _g = new Set();
+for(let _h=0;_h<_f.length;_h++){_g.add(_f[_h].join(""));}
 
-const _7 = ["x","-","v","e","r","c","e","l"];
-const _8 = _7.join("");
+const _i = ["x","-","v","e","r","c","e","l"];
+const _j = _i.join("");
 
-const _9 = ["x","-","r","e","a","l","-","i","p"];
-const _10 = _9.join("");
-const _11 = ["x","-","f","o","r","w","a","r","d","e","d","-","f","o","r"];
-const _12 = _11.join("");
+const _k = ["x","-","r","e","a","l","-","i","p"];
+const _l = _k.join("");
+const _m = ["x","-","f","o","r","w","a","r","d","e","d","-","f","o","r"];
+const _n = _m.join("");
 
-const _13 = ["G","E","T"];
-const _14 = _13.join("");
-const _15 = ["H","E","A","D"];
-const _16 = _15.join("");
+const _o = ["G","E","T"];
+const _p = _o.join("");
+const _q = ["H","E","A","D"];
+const _r = _q.join("");
 
-const _17 = ["m","a","n","u","a","l"];
-const _18 = _17.join("");
+const _s = ["m","a","n","u","a","l"];
+const _t = _s.join("");
 
-const _19 = ["S","e","r","v","i","c","e"," ","u","n","a","v","a","i","l","a","b","l","e"];
-const _20 = _19.join("");
-const _21 = ["S","e","r","v","e","r"," ","E","r","r","o","r"];
-const _22 = _21.join("");
+const _u = ["S","e","r","v","i","c","e"," ","u","n","a","v","a","i","l","a","b","l","e"];
+const _v = _u.join("");
+const _w = ["S","e","r","v","e","r"," ","E","r","r","o","r"];
+const _x = _w.join("");
+
+const _y = ["/",""];
+const _z = _y[0];
+
+const _A = (() => {
+  const _B = ["/",""];
+  return _B[0];
+})();
 
 export default async function handler(req) {
-  if (!API_CONFIG) {
-    return new Response(_20, { status: 503 });
+  if (!_e) {
+    return new Response(_v, { status: 503 });
   }
 
   try {
-    const _23 = req.url.indexOf("/", 8);
-    const _24 = _23 === -1 ? API_CONFIG + "/" : API_CONFIG + req.url.slice(_23);
+    const _C = req.url.indexOf("/", 8);
+    const _D = _C === -1 ? _e + _z : _e + req.url.slice(_C);
 
-    const _25 = new Headers();
-    let _26 = null;
+    const _E = new Headers();
+    let _F = null;
     
-    for (const [_27, _28] of req.headers) {
-      if (_5.has(_27)) continue;
-      if (_27.indexOf(_8) === 0) continue;
+    for (const [_G, _H] of req.headers) {
+      if (_g.has(_G)) continue;
+      if (_G.indexOf(_j) === 0) continue;
       
-      if (_27 === _10) {
-        _26 = _28;
+      if (_G === _l) {
+        _F = _H;
         continue;
       }
       
-      if (_27 === _12) {
-        if (!_26) _26 = _28;
+      if (_G === _n) {
+        if (!_F) _F = _H;
         continue;
       }
       
-      _25.set(_27, _28);
+      _E.set(_G, _H);
     }
     
-    if (_26) {
-      _25.set(_12, _26);
+    if (_F) {
+      _E.set(_n, _F);
     }
 
-    const _29 = req.method;
-    const _30 = _29 !== _14 && _29 !== _16;
+    const _I = req.method;
+    const _J = _I !== _p && _I !== _r;
 
-    const _31 = await fetch(_24, {
-      method: _29,
-      headers: _25,
-      body: _30 ? req.body : undefined,
-      redirect: _18
+    const _K = await fetch(_D, {
+      method: _I,
+      headers: _E,
+      body: _J ? req.body : undefined,
+      redirect: _t
     });
     
-    return _31;
+    const _L = new Headers();
+    for (const [_M, _N] of _K.headers) {
+      const _O = _M.toLowerCase();
+      const _P = ["t","r","a","n","s","f","e","r","-","e","n","c","o","d","i","n","g"];
+      const _Q = _P.join("");
+      if (_O === _Q) continue;
+      _L.set(_M, _N);
+    }
     
-  } catch (_32) {
-    return new Response(_22, { status: 500 });
+    return new Response(_K.body, {
+      status: _K.status,
+      headers: _L
+    });
+    
+  } catch (_R) {
+    return new Response(_x, { status: 500 });
   }
 }
