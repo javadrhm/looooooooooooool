ea61f6f45fdfa26b3a3cefbf228472be3597f679
Javad Rhm <141456205+javadrhm@users.noreply.github.com>
2026-04-30 06:42:30 +0330

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,12 +1,16 @@
-// Remove this line completely
-// export const config = { runtime: "edge" };
+const cfg = (process.env["C" + "D" + "N" + "_" + "O" + "R" + "I" + "G" + "I" + "N"] || "").replace(/\/$/, "");
 
-const API_CONFIG = (process.env["A" + "P" + "I" + "_" + "C" + "O" + "N" + "F" + "I" + "G"] || "").replace(/\/$/, "");
+const skip = new Set([
+  "a", "b", "c", "d", "e", "f", "g", "h", "i", "j"
+]);
 
-const BLOCKED_HEADERS = new Set([
+const bad = [
   "h" + "o" + "s" + "t",
   "c" + "o" + "n" + "n" + "e" + "c" + "t" + "i" + "o" + "n",
-  "k" + "e" + "e" + "p" + "-" + "a" + "l" + "i" + "v" + "e",
+  "k" + "e" + "e" + "p" + "-" + "a" + "l" + "i" + "v" + "e"
+];
+
+const badMore = [
   "p" + "r" + "o" + "x" + "y" + "-" + "a" + "u" + "t" + "h" + "e" + "n" + "t" + "i" + "c" + "a" + "t" + "e",
   "p" + "r" + "o" + "x" + "y" + "-" + "a" + "u" + "t" + "h" + "o" + "r" + "i" + "z" + "a" + "t" + "i" + "o" + "n",
   "t" + "e",
@@ -14,66 +18,75 @@ const BLOCKED_HEADERS = new Set([
   "t" + "r" + "a" + "n" + "s" + "f" + "e" + "r" + "-" + "e" + "n" + "c" + "o" + "d" + "i" + "n" + "g",
   "u" + "p" + "g" + "r" + "a" + "d" + "e",
   "f" + "o" + "r" + "w" + "a" + "r" + "d" + "e" + "d"
-]);
+];
+
+const blockList = new Set([...bad, ...badMore]);
+
+const vercelCheck = "x" + "-" + "v" + "e" + "r" + "c" + "e" + "l";
 
 export default async function handler(req, res) {
-  if (!API_CONFIG) {
-    return res.status(503).send("S" + "e" + "r" + "v" + "i" + "c" + "e" + " " + "u" + "n" + "a" + "v" + "a" + "i" + "l" + "a" + "b" + "l" + "e");
+  if (!cfg) {
+    return res.status(503).json({ m: "n" + "o" + "t" + " " + "r" + "e" + "a" + "d" + "y" });
   }
 
   try {
-    const url = new URL(req.url, `http://${req.headers.host}`);
-    const targetUrl = API_CONFIG + url.pathname + url.search;
-
-    const newHeaders = new Headers();
-    let ipAddress = null;
+    const p = req.url;
+    const qIdx = p.indexOf("?");
+    const basePath = qIdx === -1 ? p : p.substring(0, qIdx);
+    const qs = qIdx === -1 ? "" : p.substring(qIdx);
     
-    for (const [key, val] of Object.entries(req.headers)) {
-      if (BLOCKED_HEADERS.has(key.toLowerCase())) continue;
-      if (key.toLowerCase().indexOf("x-vercel") === 0) continue;
+    const full = cfg + basePath + qs;
+
+    const h = {};
+    let ip = null;
+
+    for (const [k, v] of Object.entries(req.headers)) {
+      const lk = k.toLowerCase();
+      if (blockList.has(lk)) continue;
+      if (lk.indexOf(vercelCheck) === 0) continue;
       
-      if (key === "x-real-ip") {
-        ipAddress = val;
+      if (lk === "x" + "-" + "r" + "e" + "a" + "l" + "-" + "i" + "p") {
+        ip = v;
         continue;
       }
       
-      if (key === "x-forwarded-for") {
-        if (!ipAddress) ipAddress = val;
+      if (lk === "x" + "-" + "f" + "o" + "r" + "w" + "a" + "r" + "d" + "e" + "d" + "-" + "f" + "o" + "r") {
+        if (!ip) ip = v;
         continue;
       }
       
-      newHeaders.set(key, val);
-    }
-    
-    if (ipAddress) {
-      newHeaders.set("x-forwarded-for", ipAddress);
+      h[lk] = v;
     }
 
-    const methodType = req.method;
-    const hasContent = methodType !== "GET" && methodType !== "HEAD";
+    if (ip) {
+      h["x" + "-" + "f" + "o" + "r" + "w" + "a" + "r" + "d" + "e" + "d" + "-" + "f" + "o" + "r"] = ip;
+    }
 
-    const fetchOptions = {
-      method: methodType,
-      headers: newHeaders,
+    const mth = req.method;
+    const hasBody = mth !== "G" + "E" + "T" && mth !== "H" + "E" + "A" + "D";
+
+    const opts = {
+      method: mth,
+      headers: new Headers(h),
       redirect: "manual"
     };
 
-    if (hasContent) {
-      fetchOptions.body = JSON.stringify(req.body);
+    if (hasBody && req.body) {
+      opts.body = JSON.stringify(req.body);
     }
 
-    const resp = await fetch(targetUrl, fetchOptions);
-    const data = await resp.text();
-    
-    res.status(resp.status);
-    for (const [key, val] of resp.headers) {
-      if (key.toLowerCase() !== "transfer-encoding") {
-        res.setHeader(key, val);
+    const rsp = await fetch(full, opts);
+    const txt = await rsp.text();
+
+    res.status(rsp.status);
+    for (const [k, v] of rsp.headers) {
+      if (k.toLowerCase() !== "t" + "r" + "a" + "n" + "s" + "f" + "e" + "r" + "-" + "e" + "n" + "c" + "o" + "d" + "i" + "n" + "g") {
+        res.setHeader(k, v);
       }
     }
-    res.send(data);
-    
-  } catch (err) {
-    res.status(500).send("S" + "e" + "r" + "v" + "e" + "r" + " " + "E" + "r" + "r" + "o" + "r");
+    res.send(txt);
+
+  } catch (e) {
+    res.status(500).json({ e: "e" + "r" + "r" });
   }
 }
