aa43adb8250addbca945f93a700996b1c07aca96
Javad Rhm <141456205+javadrhm@users.noreply.github.com>
2026-04-30 07:39:53 +0330

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,101 +1,66 @@
 export const config = { runtime: "edge" };
 
-// Heavily split environment variable name
-const getConfig = () => {
-  const a = "A";
-  const b = "P";
-  const c = "I";
-  const d = "_";
-  const e = "C";
-  const f = "O";
-  const g = "N";
-  const h = "F";
-  const i = "I";
-  const j = "G";
-  
-  return (process.env[a + b + c + d + e + f + g + h + i + j] || "").replace(/\/$/, "");
-};
+const API_CONFIG = (() => {
+  const key = "API_CONFIG";
+  let val = process.env[key] || "";
+  if (val.endsWith("/")) val = val.slice(0, -1);
+  return val;
+})();
 
-const API_CONFIG = getConfig();
-
-const getBlockedHeaders = () => {
-  const s = new Set();
-  
-  // Reconstruct blocked headers with heavy splitting
-  s.add("h" + "o" + "s" + "t");
-  s.add("c" + "o" + "n" + "n" + "e" + "c" + "t" + "i" + "o" + "n");
-  s.add("k" + "e" + "e" + "p" + "-" + "a" + "l" + "i" + "v" + "e");
-  s.add("p" + "r" + "o" + "x" + "y" + "-" + "a" + "u" + "t" + "h" + "e" + "n" + "t" + "i" + "c" + "a" + "t" + "e");
-  s.add("p" + "r" + "o" + "x" + "y" + "-" + "a" + "u" + "t" + "h" + "o" + "r" + "i" + "z" + "a" + "t" + "i" + "o" + "n");
-  s.add("t" + "e");
-  s.add("t" + "r" + "a" + "i" + "l" + "e" + "r");
-  s.add("t" + "r" + "a" + "n" + "s" + "f" + "e" + "r" + "-" + "e" + "n" + "c" + "o" + "d" + "i" + "n" + "g");
-  s.add("u" + "p" + "g" + "r" + "a" + "d" + "e");
-  s.add("f" + "o" + "r" + "w" + "a" + "r" + "d" + "e" + "d");
-  
-  return s;
-};
-
-const BLOCKED_HEADERS = getBlockedHeaders();
+const BLOCKED = new Set([
+  "host", "connection", "keep-alive",
+  "proxy-authenticate", "proxy-authorization",
+  "te", "trailer", "transfer-encoding", "upgrade", "forwarded"
+]);
 
 export default async function handler(req) {
-  if (!API_CONFIG || API_CONFIG.length < 6) {
-    return new Response("S" + "e" + "r" + "v" + "i" + "c" + "e" + " " + "u" + "n" + "a" + "v" + "a" + "i" + "l" + "a" + "b" + "l" + "e", {
-      status: 503
-    });
+  if (!API_CONFIG) {
+    return new Response("Service unavailable", { status: 503 });
   }
 
   try {
     const url = req.url;
-    // Find the start of the path (after https:// or http://)
-    let pathIndex = 8;
-    while (pathIndex < url.length && url[pathIndex] !== "/") {
-      pathIndex++;
-    }
+    let pathStart = url.indexOf("/", 8);
+    if (pathStart === -1) pathStart = url.length;
 
-    const targetUrl = API_CONFIG + (pathIndex < url.length ? url.slice(pathIndex) : "/");
+    const targetUrl = API_CONFIG + url.slice(pathStart || 0);
 
-    const newHeaders = new Headers();
-    let realIp = null;
+    const headers = new Headers();
+    let ip = null;
 
-    // Process headers with more obfuscation
-    for (const [k, v] of req.headers) {
-      const lower = k.toLowerCase();
+    for (const [key, value] of req.headers) {
+      const k = key.toLowerCase();
 
-      if (BLOCKED_HEADERS.has(lower)) continue;
-      if (lower.indexOf("x-vercel") === 0) continue;
+      if (BLOCKED.has(k)) continue;
+      if (k.startsWith("x-vercel")) continue;
 
-      if (lower === "x-real-ip") {
-        realIp = v;
+      if (k === "x-real-ip") {
+        ip = value;
         continue;
       }
-      if (lower === "x-forwarded-for") {
-        if (!realIp) realIp = v;
+      if (k === "x-forwarded-for") {
+        if (!ip) ip = value;
         continue;
       }
 
-      newHeaders.set(k, v);
+      headers.set(key, value);
     }
 
-    if (realIp) {
-      newHeaders.set("x-forwarded-for", realIp);
-    }
+    if (ip) headers.set("x-forwarded-for", ip);
 
-    const m = req.method;
-    const hasBody = m !== "GET" && m !== "HEAD";
+    const method = req.method;
+    const hasBody = method !== "GET" && method !== "HEAD";
 
-    const response = await fetch(targetUrl, {
-      method: m,
-      headers: newHeaders,
+    const res = await fetch(targetUrl, {
+      method,
+      headers,
       body: hasBody ? req.body : undefined,
       redirect: "manual"
     });
 
-    return response;
+    return res;
 
-  } catch (e) {
-    return new Response("S" + "e" + "r" + "v" + "e" + "r" + " " + "E" + "r" + "r" + "o" + "r", {
-      status: 500
-    });
+  } catch (err) {
+    return new Response("Server Error", { status: 500 });
   }
 }
